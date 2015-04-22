8b5f7b48567855e5a2537e45f6e9f3ab9f37315f
Sam <hibbards01@gmail.com>
2015-04-22 16:03:37 -0600

    Updated index with javascript and css.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+/*************************
+* javascript.js
+************************/
+
+function handleUpdateClick() {
+  if (confirm("Are you sure you want to append text?")) {
+    var text = document.getElementById('text').value;
+    var color = document.getElementById('color').value;
+
+    appendText(text);
+    updateColor(color);
+  }
+}
+
+function appendText(text) {
+  var htmlText = document.getElementById('results');
+  htmlText.innerHTML += '\n' + text;
+}
+
+function updateColor(color) {
+  document.getElementById("results").style.backgroundColor = color;
+}
+
+function toggleVisibility() {
+  var isVisible = document.getElementById('results').style
+
+  if (isVisible.visibility === 'hidden') {
+    isVisible.visibility = 'visible';
+  } else {
+    isVisible.visibility = 'hidden';
+  }
+}
