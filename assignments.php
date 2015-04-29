aa50848e202611d71e33a1a3deace705a1c5b87e
Sam <hibbards01@gmail.com>
2015-04-29 13:19:15 -0600

    Started the profile page.

--- assignments.php
+++ assignments.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
-<!-- index.html -->
+<!-- assignments.html -->
 <html>
 <head>
-  <title>Home Page</title>
+  <title>Assignments</title>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
   <?php include_once('header.php') ?>
   <div class="container">
     <div class="jumbotron">
-      <h1>Samuel Hibbard</h1>
+      <h1>Assignments</h1>
     </div>
   </div>
 </body>
---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+  // Grab the url!
+  $page = $_SERVER['REQUEST_URI'];
+  $isProfile = true;  // Defualt
+
+  // Now change which page is active!
+  if ($page === '/php/index.php') {
+    $isProfile = true;
+  } else {
+    $isProfile = false;
+  }
+?>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Sam's Website</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li <?php echo ($isProfile == true) ? "class='active'" : ""; ?>>
+          <a href="index.php">Profile</a>
+        </li>
+        <li <?php echo ($isProfile == false) ? "class='active'" : ""; ?>>
+          <a href="assignments.php">Assignments</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
