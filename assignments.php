34067ca8a2cd86e358b1dfa55d3852d66ad9b869
Sam <hibbards01@gmail.com>
2015-04-29 16:17:25 -0600

    Had to change header.

--- assignments.php
+++ assignments.php
@@ -5,6 +5,7 @@
   <title>Assignments</title>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
