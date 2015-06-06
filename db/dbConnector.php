e895eb7a64d3512d0e87f2c350d0275ab5ea58f4
Sam <hibbards01@gmail.com>
2015-06-06 12:04:28 -0600

    Error testing 6

--- db/dbConnector.php
+++ db/dbConnector.php
@@ -18,19 +18,19 @@
       require ("setUpLocalHostCredentials.php");
     } else {
       // We are on the openshift database!
-      // echo "<h1>Openshift</h1>";
+      echo "<h1>Openshift</h1>";
       $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
       $dbPort = ":" . getenv('OPENSHIFT_MYSQL_DB_PORT');
       $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
       $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
     }
 
-    // echo "host:$dbHost$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+    echo "host:$dbHost$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 
     try {
       // Connect to the database!
       $db = new PDO("mysql:host=$dbHost$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-      // echo "<p>Success</p>";
+      echo "<p>Success</p>";
     } catch (PDOException $e) {
       echo "ERROR in connecting to database: " . $e->getMessage();
       die();
