78be04f900ae12b3962ff8f2634ceddf138bd300
Sam <hibbards01@gmail.com>
2015-06-06 13:10:19 -0600

    Error testing 8

--- ecen_project/insert_into.php
+++ ecen_project/insert_into.php
@@ -24,6 +24,7 @@
     $psswrd = $_POST["password"];
 
     $passHash = password_hash($psswrd, PASSWORD_DEFAULT);
+    echo "DEBUG00";
     // Now create the sql statement
     $stmt;
     if (isset($_POST['is_faculty'])) {
